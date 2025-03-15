139ee9559c553ff8bf23d15e71f93043ec747c6b
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2025-03-15 23:16:29 +0700

    simple-git

---
+++ index.js
@@ -0,0 +1,46 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+
+const file = "./data.json";
+
+const isValidDate = (date) => {
+    const startDate = moment("2024-01-01", "YYYY-MM-DD");
+    const endDate = moment("2025-03-14", "YYYY-MM-DD");
+    return date.isBetween(startDate, endDate, null, "[]");
+};
+
+const markCommit = async (date) => {
+    const data = { date: date.toISOString() };
+    await jsonfile.writeFile(file, data);
+
+    const git = simpleGit();
+    await git.add([path]);
+    await git.commit(date.toISOString(), { "--date": date.toISOString() });
+};
+
+const makeCommits = async (n) => {
+    const git = simpleGit();
+
+    for (let i = 0; i < n; i++) {
+        const randomWeeks = random.int(0, 54*4);
+        const randomDays = random.int(0, 6);
+
+        const randomDate = moment("2024-01-01", "YYYY-MM-DD")
+            .add(randomWeeks, "weeks")
+            .add(randomDays, "days");
+
+        if (isValidDate(randomDate)) {
+            console.log(`Creating commit: ${randomDate.toISOString()}`);
+            await markCommit(randomDate);
+        }else{
+            console.log(`Invalid date: ${randomDate.toISOString()}, skipping...`);
+        }
+    }
+
+    console.log("Pushing all commits...");
+    await git.push();
+}; 
+
+makeCommits(50000);
